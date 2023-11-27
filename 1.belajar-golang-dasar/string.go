064937146324a0fd3bbe87d60ca790781adf3e90
izulhsr <izulhsr@gmail.com>
2023-11-27 14:12:34 +0700

    string

---
+++ 1.belajar-golang-dasar/string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Jhon")
+	fmt.Println("Jhon Smith")
+	fmt.Println("Jhon Smith Lance")
+
+	fmt.Println(len("Jhon"))
+	fmt.Println("Jhon Smith"[0])
+	fmt.Println("Jhon Smith Lance"[1])
+}
