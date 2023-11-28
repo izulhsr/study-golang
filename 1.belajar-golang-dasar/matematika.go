46e8d094cbb7849fcd82d5fa7a362b32db8d5e16
izulhsr <izulhsr@gmail.com>
2023-11-28 23:09:23 +0700

    operasi matematika

---
+++ 1.belajar-golang-dasar/matematika.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Operasi metematika
+	a := 10
+	b := 5
+	c := a + b
+	println(c)
+	// augmented assigement
+	a += 10
+	fmt.Println(a)
+	// unary operator
+	z := 0
+	z++
+	z++
+	z++
+	fmt.Println("Z = ", z)
+}
