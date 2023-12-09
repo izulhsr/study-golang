acacb7bcf20545ce683b97a502b83143c3379686
izulhsr <izulhsr@gmail.com>
2023-12-09 13:03:14 +0700

    panic

--- 1.belajar-golang-dasar/panic.go
+++ 1.belajar-golang-dasar/panic.go
@@ -4,12 +4,18 @@ import "fmt"
 
 func endApp() {
 	fmt.Println("End App")
+	// recover benar
+	message := recover()
+	fmt.Println("Pesan Erorr", message)
 }
 func runApp(error bool) {
 	defer endApp()
 	if error {
 		panic("Yah Error")
 	}
+	// Recover salah
+	// message := recover()
+	// fmt.Println("Pesan Erorr", message)
 }
 
 func main() {
