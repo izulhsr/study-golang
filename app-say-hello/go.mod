4add1e2ef5cbf60b446fa58b8f5255335c359ef5
izulhsr <izulhsr@gmail.com>
2023-12-15 10:37:39 +0700

    major update

--- app-say-hello/go.mod
+++ app-say-hello/go.mod
@@ -2,4 +2,4 @@ module app-say-hello
 
 go 1.21.4
 
-require github.com/izulhsr/go-say-hello v1.5.0
+require github.com/izulhsr/go-say-hello/v2 v2.0.0 // indirect
--- app-say-hello/main.go
+++ app-say-hello/main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 
-	gosayhello "github.com/izulhsr/go-say-hello"
+	gosayhello "github.com/izulhsr/go-say-hello/v2"
+	// gosayhello "github.com/izulhsr/go-say-hello"
 )
 
 func main() {
-	fmt.Println(gosayhello.SayHello())
+	fmt.Println(gosayhello.SayHello("joko"))
 }
