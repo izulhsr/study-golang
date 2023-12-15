54d862bc793baa99179bec9997652f8f6a22849d
izulhsr <izulhsr@gmail.com>
2023-12-15 09:46:49 +0700

    menambah dependency

---
+++ app-say-hello/go.mod
@@ -0,0 +1,5 @@
+module app-say-hello
+
+go 1.21.4
+
+require github.com/izulhsr/go-say-hello v1.0.0 // indirect
---
+++ app-say-hello/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	gosayhello "github.com/izulhsr/go-say-hello"
+)
+
+func main() {
+	fmt.Println(gosayhello.SayHello())
+}
