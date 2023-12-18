5232d43d3dd57eda9976d4309732d75eb9f3920e
izulhsr <izulhsr@gmail.com>
2023-12-18 08:24:59 +0700

    before dan after test

--- belajar-golang-unit-test/helper/hello_world_test.go
+++ belajar-golang-unit-test/helper/hello_world_test.go
@@ -9,6 +9,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestMain(m *testing.M) {
+	fmt.Println("Before Test")
+	m.Run()
+	fmt.Println("After Test")
+}
+
 func TestSkip(t *testing.T) {
 	if runtime.GOOS == "windows" {
 		t.Skip("Tdk jalan di windows")
