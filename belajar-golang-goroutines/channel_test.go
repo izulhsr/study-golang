583633858e4a084075f394495658cc253133cf6f
izulhsr <izulhsr@gmail.com>
2023-12-19 07:59:13 +0700

    membuat channel

---
+++ belajar-golang-goroutines/channel_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestCreateChannel(t *testing.T) {
+	chanel := make(chan string)
+	defer close(chanel)
+	go func() {
+		time.Sleep(2 * time.Second)
+		chanel <- "Jhon Smith Lance"
+		fmt.Println("Selesai Mengirim Data ke Chanel")
+	}()
+	data := <-chanel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+}
