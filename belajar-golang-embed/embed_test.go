a077418725cd773c722965f9f73929d16b429cb4
izulhsr <izulhsr@gmail.com>
2024-01-04 08:02:10 +0700

    embed file ke byte

--- belajar-golang-embed/embed_test.go
+++ belajar-golang-embed/embed_test.go
@@ -3,6 +3,8 @@ package belajargolangembed
 import (
 	_ "embed"
 	"fmt"
+	"io/fs"
+	"io/ioutil"
 	"testing"
 )
 
@@ -11,4 +13,14 @@ var embed string
 
 func TestString(t *testing.T) {
 	fmt.Println(embed)
+}
+
+//go:embed logo.png
+var logo []byte
+
+func TestLogo(t *testing.T) {
+	err := ioutil.WriteFile("logo_new.png", logo, fs.ModePerm)
+	if err != nil {
+		panic(err)
+	}
 }
